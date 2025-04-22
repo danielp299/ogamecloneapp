3c6237604816a4d2b5a1912af57b66547c1ba061
danielp299 <danielp299@gmail.com>
2025-04-22 22:44:38 +0000

    building con accion

--- myapp.Tests/BuildingsPageTests.cs
+++ myapp.Tests/BuildingsPageTests.cs
@@ -1,4 +1,5 @@
 //csharp
+using AngleSharp.Dom;
 using Bunit;
 using myapp.Components.Pages;
 using Xunit;
@@ -7,10 +8,62 @@ namespace myapp.Tests.Components.Pages
 {
     public class BuildingsPageTests : Bunit.TestContext
     {
-        [Fact]
+       [Fact]
         public void BuildingsPage_Render_Should_Render_Page()
         {
-            RenderComponent<BuildingsPage>();
+            var cut = RenderComponent<BuildingsPage>();
         }
+
+        [Fact]
+        public void BuildingsPage_Render_Should_Render_Construct_Button_For_Level_0()
+        {
+            Guid buildingId = Guid.NewGuid();           
+            var building = new BuildingsPage.BuildingDto { Level = 0, Id = buildingId, ConstructionDuration = TimeSpan.FromSeconds(2)};
+            var cut = RenderComponent<BuildingsPage>(ComponentParameter.CreateParameter(nameof(BuildingsPage.Buildings), new List<BuildingsPage.BuildingDto>() { building }));
+
+            AssertButtonIsRendered($"#construct-building-{building.Id}", cut);
+        }
+
+        [Fact]
+        public void BuildingsPage_Dismantle_Hides_Buttons()
+        {
+            Guid buildingId = Guid.NewGuid();
+            var building = new BuildingsPage.BuildingDto { Level = 1, Id = buildingId , ConstructionDuration = TimeSpan.FromSeconds(2)};
+            var cut = RenderComponent<BuildingsPage>(ComponentParameter.CreateParameter(nameof(BuildingsPage.Buildings), new List<BuildingsPage.BuildingDto>() { building }));
+
+            AssertButtonIsRendered($"#dismantle-building-{building.Id}", cut);            
+            AssertButtonIsNotRendered($"#cancel-building-{building.Id}", cut);
+        }
+
+        [Fact]
+        public void BuildingsPage_Render_Should_Render_Dismantle_And_Upgrade_Buttons_For_Level_1()
+        {
+
+            Guid buildingId = Guid.NewGuid();
+            var building = new BuildingsPage.BuildingDto { Level = 1, Id = buildingId };
+            var cut = RenderComponent<BuildingsPage>(ComponentParameter.CreateParameter(nameof(BuildingsPage.Buildings), new List<BuildingsPage.BuildingDto>() { building }));
+
+            AssertButtonIsRendered($"#dismantle-building-{building.Id}", cut);
+            AssertButtonIsRendered($"#upgrade-building-{building.Id}", cut);
+        }
+
+       
+        private string GetCancelId(BuildingsPage.BuildingDto building)
+        {
+            return $"#cancel-building-{building.Id}";
+        }
+
+
+        private void AssertButtonIsRendered(string id, IRenderedComponent<BuildingsPage> cut)
+        {
+            var button = cut.Find(id);
+            Assert.NotNull(button);
+        }
+         private void AssertButtonIsNotRendered(string id, IRenderedComponent<BuildingsPage> cut)
+        {            
+            Assert.Throws<Bunit.ElementNotFoundException>(() => cut.Find(id));
+        }
+
+       
     }
 }
